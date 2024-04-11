e4ebc66a27de90e51413f94735f597e3c79880c9
@Rinkesh_Epesign375 <rinkesh@epesign.com>
2024-04-11 23:58:53 +0530

    new added data update in UI by invalid success

--- src/app/add-hero/page.jsx
+++ src/app/add-hero/page.jsx
@@ -1,21 +1,59 @@
-"use client"
+"use client";
 import React from "react";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery ,useQueryClient} from "@tanstack/react-query";
 import axios from "axios";
 
-const hero = { name: "Rinkesh", alterEgo: "Full Stack Developer" };
-
-const addNewHero = async (data) => {
-    return await axios.post(`http://localhost:8080/superheroes`,data)
+const hero = {
+  name: "Rinkesh",
+  alterEgo: "Full Stack Developer",
 };
 
-const AddHero = () => {
-  const { isLoading, isError, error ,mutate,isSuccess} = useMutation({ mutationFn:addNewHero,onSuccess:()=>console.log("Data added successfully") });
-  console.log(isSuccess,"-------------")
-  const handleAddNewDataButtonClick = ()=>{
-    mutate(hero)
+const addNewHero = async (data) => {
+  return await axios.post(`http://localhost:8080/superheroes`, data);
+};
+
+async function fetchSuperHero() {
+  try {
+    const response = await axios.get(`http://localhost:8080/superheroes`);
+    return response;
+  } catch (error) {
+    throw new Error("Failed to fetch superheroes");
   }
-  return <button onClick={handleAddNewDataButtonClick}>Add Data</button>;
+}
+
+const AddHero = () => {
+    const queryClient = useQueryClient();
+  const { isLoading, isError, error, mutate, isSuccess } = useMutation({
+    mutationFn: addNewHero,
+    onSuccess: () => {
+        //This onSuccess code will update the UI or we can say it will show the new added data by doing invalidte queryClient.invalidateQueries("super-hero-add-page")
+        return queryClient.invalidateQueries("super-hero-add-page")
+    },
+  });
+
+  const {
+    data,
+    isError: superError,
+    error: superErrorMessage,
+    isLoading: superHeroLoading,
+  } = useQuery({
+    queryKey: ["super-hero-add-page"],
+    queryFn: fetchSuperHero,
+  });
+
+  const handleAddNewDataButtonClick = () => {
+    mutate(hero);
+  };
+  return (
+    <>
+      <div>
+        <button onClick={handleAddNewDataButtonClick}>Add Data</button>
+      </div>
+      {data?.data.map((hero) => (
+        <h1 key={hero.id}>{hero?.name}</h1>
+      ))}
+    </>
+  );
 };
 
 export default AddHero;
